da7c9cddd8e777eb7c194eb3138773462f29fc63
Andrew Kemp <akemp850@gmail.com>
2018-05-03 09:59:03 +0100

    added NoteController - spiked, not tested

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,20 @@
-// var test = document.getElementById('app');
-// console.log(test);
-var test = document.getElementById('app');
-var text = 'howdy';
 
-test.innerHTML = text;
+// var test = document.getElementById('app');
+// var text = 'howdy';
+//
+// test.innerHTML = text;
+
+(function(exports){
+  function NoteController(notelist = new NoteList()){
+    this.notelist = notelist;
+    notelist.createAndAddNote("Favourite drink: seltzer");
+    this.listView = new NoteListView(notelist);
+  };
+
+  NoteController.prototype.addHtmlToPage = function(){
+    var noteText = this.listView.listingNotes();
+    (document.getElementById('app')).innerHTML = noteText;
+  };
+
+  exports.NoteController = NoteController;
+})(this);
